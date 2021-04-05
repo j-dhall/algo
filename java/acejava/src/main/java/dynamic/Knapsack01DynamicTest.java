5daabe7f3c4bfbaa8331ed7fbdc5fcbf936e0362
j-dhall <74283389+j-dhall@users.noreply.github.com>
2021-04-05 17:39:58 +0530

    yet to implement space eficient knapsack01, this is just a file

--- java/acejava/src/main/java/dynamic/Knapsack01DynamicTest.java
+++ java/acejava/src/main/java/dynamic/Knapsack01DynamicTest.java
@@ -9,10 +9,27 @@ import dynamic.Knapsack01Dynamic.SetProfit;
 
 class Knapsack01DynamicTest {
 
+	/*
+	 * https://www.educative.io/module/lesson/dynamic-programming-patterns/7AQm4v6r1y8
+	 */
+	//private int[] profits = {1, 6, 10, 16};
+	//private int[] weights = {1, 2, 3, 5};
+	//private int[][] capacitiesProfits = {{7, 22}, {6, 17}}; //{{capacity, profit}, {capacity, profit}}
 
-	private int[] profits = {1, 6, 10, 16};
-	private int[] weights = {1, 2, 3, 5};
-	private int[][] capacitiesProfits = {{7, 22}, {6, 17}}; //{{capacity, profit}, {capacity, profit}}
+	/*
+	 * Items: { Apple, Orange, Banana, Melon }
+	 * Weights: { 2, 3, 1, 4 }
+	 * Profits: { 4, 5, 3, 7 }
+	 * Knapsack capacity: 5
+	 * Apple + Orange (total weight 5) => 9 profit
+	 * Apple + Banana (total weight 3) => 7 profit
+	 * Orange + Banana (total weight 4) => 8 profit
+	 * Banana + Melon (total weight 5) => 10 profit
+	 */
+	private int[] profits = {4,5,3,7};//{1, 6, 10, 16};
+	private int[] weights = {2,3,1,4};//{1, 2, 3, 5};
+	private int[][] capacitiesProfits = {{5, 10}};//{{7, 22}, {6, 17}}; //{{capacity, profit}, {capacity, profit}}
+
 	Knapsack01Dynamic knapsack01;
 
 	@BeforeEach
