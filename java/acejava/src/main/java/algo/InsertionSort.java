32199fe0456e8cee91865aec3a9e0f3ed7bd1b72
j-dhall <74283389+j-dhall@users.noreply.github.com>
2021-04-08 16:43:13 +0530

    insertion sort

---
+++ java/acejava/src/main/java/algo/InsertionSort.java
@@ -0,0 +1,21 @@
+package algo;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+	public void sort (int[] items) {
+		//starting from 2nd element till last element
+		//for each element, keep swapping it with its left element
+		//as long as the left element is greater.
+		for (int i = 1; i < items.length; i++) {
+			//System.out.printf("Iter %d: %s\n", i+1, Arrays.toString(items));
+			for (int j = i; j > 0; j--) {
+				if (items[j-1] > items[j]) {
+					int temp = items[j-1];
+					items[j-1] = items[j]; //move left
+					items[j] = temp;
+				}
+			}
+		}
+	}
+}
---
+++ java/acejava/src/main/java/algo/InsertionSortTest.java
@@ -0,0 +1,33 @@
+package algo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import util.ArrayUtil;
+
+class InsertionSortTest {
+	int[] items;
+	int nItems, maxVal;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		nItems = 10;
+		maxVal = 100;
+		items = ArrayUtil.getIntArray(nItems, maxVal);
+	}
+
+	@Test
+	void testSort() {
+		InsertionSort sorter = new InsertionSort();
+		//System.out.printf("Before: %s\n", Arrays.toString(items));
+		sorter.sort(items);
+		//System.out.printf("After:  %s\n", Arrays.toString(items));
+		for (int i = 0; i < items.length - 1; i++) {
+			assertTrue(items[i] <= items[i+1]);
+		}
+	}
+}
